64a917f13b7a135f58de2c80a0134345b686d75a
pavan-aeturi <f20180212@goa.bits-pilani.ac.in>
2019-08-13 00:44:13 +0530

    newone

--- src/whattheasd/whattheasd.java
+++ src/whattheasd/whattheasd.java
@@ -3,7 +3,8 @@ package whattheasd;
 public class whattheasd {
 	public static void main(String args[])
 	{
-		System.out.println("it is easy")
+		System.out.println("it is easy");
+		System.out.println("crazy");
 	}
 
 }
